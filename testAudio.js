5843ca7c59694ce88c71472223fb8bea544673fc
Rodrigo Savage <pi@raspberrypi.(none)>
2015-01-02 18:07:26 +0000

    change to omxplayer

--- testAudio.js
+++ testAudio.js
@@ -7,35 +7,62 @@ var volume = 0;
 // var songIsPlaying = false;
 var playSong = function(song){
 	currentSong = song;
-	child = child_process.spawn(
-		// 'pwd',
-		'python', ['/Users/rodrigosavage/Documents/phd/hydroponic/restPi/python/audio.py',song],
-	 	// 'omxplayer',[song],
-	 	{
-		 	detached:true,
-		    stdio: [
-		      'pipe', // use parents stdin for child
-		      'pipe', // pipe child's stdout to parent
-		      fs.openSync("err.out", "w") // direct child's stderr to a file
-		    ]
-		}
-	);
-	inputStream = child.stdin;
-	child.stdout.on('data', function(data) { console.log(data.toString('ascii')); });
+	try{
+		child = child_process.spawn(
+			// 'pwd',
+			//'python', ['/Users/rodrigosavage/Documents/phd/hydroponic/restPi/python/audio.py',song],
+			'omxplayer',[song],
+			{
+				detached:true,
+					stdio: [
+						'pipe', // use parents stdin for child
+						'pipe', // pipe child's stdout to parent
+						fs.openSync("err.out", "w") // direct child's stderr to a file
+					]
+			}
+		);
+	}
+	catch(ex){
+		console.log("Error while playing song");
+		console.log(ex);
+	}
+	try{
+		inputStream = child.stdin;
+		inputStream.on('error',function(err){
+			console.log('error',err);
+			inputStream = undefined;
+		});
+		child.stdout.on('data', function(data) { 
+			console.log("data");
+			console.log(data.toString('ascii')); 
+		});
 
-	child.on('error',function(err){
-		inputStream = undefined;
-		console.log(err);
-	})
-	child.on('exit',function(){
-		inputStream = undefined;
-		console.log('Termino el proceso');
-	})
+		child.on('error',function(err){
+			inputStream = undefined;
+			console.log("Error while playing song");
+			console.log(err);
+		})
+		child.on('close',function(){
+			inputStream = undefined;
+			console.log('Termino el proceso');
+		})
+		child.on('disconnect',function(){
+			inputStream = undefined;
+			console.log('Termino el proceso');
+		})
+		child.on('exit',function(){
+			inputStream = undefined;
+			console.log('Termino el proceso');
+		})
+	}catch(e){
+		console.log(e);
+	}
 	return {song:song,volume:0};
 }
 exports.playSong = playSong;
 
 var incVolume = function(amount){
+	console.log('incVol');
 	volume += parseFloat(amount);
 	count = Math.abs(amount)+1;
 	var s;
@@ -46,7 +73,8 @@ var incVolume = function(amount){
 		s = Array(count).join("-")
 	}
 	if(inputStream!==undefined){
-		inputStream.write("o");
+			console.log('Mando volumen');
+			inputStream.write(s);
 	}
 	return volume;
 }
